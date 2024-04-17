6cac03559c57c57a60ce5629a10e0ab41d55a312
OscarMadera1 <omaderanegrete@gmail.com>
2024-04-17 15:59:30 -0500

    modificaciones en url

--- apirest/apirest/urls.py
+++ apirest/apirest/urls.py
@@ -6,5 +6,5 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("api.urls")),
-    path('docs/',include_docs_urls(title='Api documentación')),
+    path('',include_docs_urls(title='Api documentación')),
 ]
